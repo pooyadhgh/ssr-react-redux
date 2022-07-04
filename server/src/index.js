6e65c473437a29759196255fb716203be433f7c7
Pooya Dehghani <pooya.dehghani@hm.com>
2022-07-04 12:42:55 +0200

    Add react to the project

--- server/src/index.js
+++ server/src/index.js
@@ -1,14 +1,19 @@
 const dotenv = require('dotenv');
 const express = require('express');
+const React = require('react');
+const renderToString = require('react-dom/server').renderToString;
+
+const Home = require('./client/components/Home').default;
 
 // .env config
 dotenv.config({ path: '../.env' });
 
 const app = express();
-const port = process.env.SERVER_PORT;
+const port = process.env.SERVER_PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  const content = renderToString(<Home />);
+  res.send(content);
 });
 
 app.listen(port, () => console.log(`⚡️ Server is running on port: ${port}`));
